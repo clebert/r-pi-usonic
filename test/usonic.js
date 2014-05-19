fe1e6ea65feb3d45c7b5f92bed2da3089f81d518
Clemens Akens <clebert@me.com>
2014-05-20 00:33:25 +0200

    Update test

--- test/usonic.js
+++ test/usonic.js
@@ -4,6 +4,24 @@ var UltrasonicSensor = require('../index').UltrasonicSensor;
 
 var ultrasonicSensor = new UltrasonicSensor(15, 14);
 
+var maxDistanceCm = -Infinity;
+var minDistanceCm = Infinity;
+
 setInterval(function () {
-    console.log(ultrasonicSensor.getDistanceCm().toFixed(2));
+    var distanceCm = ultrasonicSensor.getDistanceCm().toFixed(2);
+
+    if (distanceCm > maxDistanceCm) {
+        maxDistanceCm = distanceCm;
+    }
+
+    if (distanceCm < minDistanceCm) {
+        minDistanceCm = distanceCm;
+    }
+
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+
+    process.stdout.write(
+        'current: ' + distanceCm + ', min: ' + minDistanceCm + ', max: ' + maxDistanceCm
+    );
 }, 60);
