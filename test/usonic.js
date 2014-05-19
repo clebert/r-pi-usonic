4062cf8a15905acaf2012762b3183ec1ef2c01d8
Clemens Akens <clebert@me.com>
2014-05-20 00:49:49 +0200

    Update test

--- test/usonic.js
+++ test/usonic.js
@@ -4,28 +4,48 @@ var UltrasonicSensor = require('../index').UltrasonicSensor;
 
 var ultrasonicSensor = new UltrasonicSensor(15, 14);
 
+function getMedian(a, b, c) {
+    return Math.max(Math.min(a, b), Math.min(Math.max(a, b), c));
+}
+
 var count = 0;
 var maxDistanceCm = -Infinity;
 var minDistanceCm = Infinity;
 
-setInterval(function () {
-    var distanceCm = ultrasonicSensor.getDistanceCm();
+function getDistanceCm() {
+    setTimeout(function () {
+        var a = ultrasonicSensor.getDistanceCm();
 
-    if (distanceCm > maxDistanceCm) {
-        maxDistanceCm = distanceCm;
-    }
+        setTimeout(function () {
+            var b = ultrasonicSensor.getDistanceCm();
 
-    if (distanceCm < minDistanceCm) {
-        minDistanceCm = distanceCm;
-    }
+            setTimeout(function () {
+                var c = ultrasonicSensor.getDistanceCm();
 
-    process.stdout.clearLine();
-    process.stdout.cursorTo(0);
+                var distanceCm = getMedian(a, b, c);
 
-    process.stdout.write(
-        'current: ' + distanceCm.toFixed(2) +
-        ', min: ' + minDistanceCm.toFixed(2) +
-        ', max: ' + maxDistanceCm.toFixed(2) +
-        ', count: ' + (count += 1)
-    );
-}, 60);
+                if (distanceCm > maxDistanceCm) {
+                    maxDistanceCm = distanceCm;
+                }
+
+                if (distanceCm < minDistanceCm) {
+                    minDistanceCm = distanceCm;
+                }
+
+                process.stdout.clearLine();
+                process.stdout.cursorTo(0);
+
+                process.stdout.write(
+                    'current: ' + distanceCm.toFixed(2) +
+                    ', min: ' + minDistanceCm.toFixed(2) +
+                    ', max: ' + maxDistanceCm.toFixed(2) +
+                    ', count: ' + (count += 1)
+                );
+
+                getDistanceCm();
+            }, 60);
+        }, 60);
+    }, 60);
+}
+
+getDistanceCm();
