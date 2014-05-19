94b0f1300cd1892f045ec1adb4fb2607ede1ba4a
Clemens Akens <clebert@me.com>
2014-05-20 00:09:39 +0200

    Update test

--- test/usonic.js
+++ test/usonic.js
@@ -5,5 +5,5 @@ var UltrasonicSensor = require('../index').UltrasonicSensor;
 var ultrasonicSensor = new UltrasonicSensor(15, 14);
 
 setInterval(function () {
-    console.log(ultrasonicSensor.getDistanceCm());
+    console.log(ultrasonicSensor.getDistanceCm().toFixed(2));
 }, 60);
