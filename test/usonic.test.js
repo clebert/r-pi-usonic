d18acd9e59e9f2629c657569dacca44dfbc754c7
Clemens Akens <clebert@me.com>
2014-05-24 12:19:04 +0200

    Update test

--- test/usonic.test.js
+++ test/usonic.test.js
@@ -2,114 +2,109 @@
 
 'use strict';
 
-process.env.R_PI_USONIC_TEST = 'true';
-
 var assert = require('extended-assert');
-var usonic = require('../index.js');
+var mock = assert.requireMock(__dirname, '../build/Release/usonic.node', {});
+var UltrasonicSensor = require('../index.js');
 
-describe('usonic', function () {
+describe('UltrasonicSensor()', function () {
 
-    describe('.UltrasonicSensor()', function () {
+    it('should throw an illegal-arguments error', function () {
+        assert.throwsError(function () {
+            return new UltrasonicSensor();
+        }, 'TypeError', '(!>number, number) => void');
+    });
 
-        it('should throw an illegal-arguments error', function () {
-            assert.throwsError(function () {
-                return new usonic.UltrasonicSensor();
-            }, 'TypeError', '(!>number, number) => void');
-        });
+    it('should throw a missing-new-keyword error', function () {
+        assert.throwsError(function () {
+            /* jshint -W064 */
 
-        it('should throw a missing-new-keyword error', function () {
-            assert.throwsError(function () {
-                usonic.UltrasonicSensor(0, 0);
-            }, 'Error', 'Missing new keyword.');
-        });
+            UltrasonicSensor(0, 0);
+        }, 'Error', 'Missing new keyword.');
+    });
 
-        it('should not throw an invalid-echo-pin error', function () {
-            var type = 'Error';
-            var message = 'Invalid echo pin.';
+    it('should not throw an invalid-echo-pin error', function () {
+        var type = 'Error';
+        var message = 'Invalid echo pin.';
 
-            assert.doesNotThrowError(function () {
-                return new usonic.UltrasonicSensor(0, 0);
-            }, type, message);
+        assert.doesNotThrowError(function () {
+            return new UltrasonicSensor(0, 0);
+        }, type, message);
 
-            assert.doesNotThrowError(function () {
-                return new usonic.UltrasonicSensor(53, 0);
-            }, type, message);
-        });
+        assert.doesNotThrowError(function () {
+            return new UltrasonicSensor(53, 0);
+        }, type, message);
+    });
 
-        it('should throw an invalid-echo-pin error', function () {
-            var type = 'Error';
-            var message = 'Invalid echo pin.';
+    it('should throw an invalid-echo-pin error', function () {
+        var type = 'Error';
+        var message = 'Invalid echo pin.';
 
-            assert.throwsError(function () {
-                return new usonic.UltrasonicSensor(-1, 0);
-            }, type, message);
+        assert.throwsError(function () {
+            return new UltrasonicSensor(-1, 0);
+        }, type, message);
 
-            assert.throwsError(function () {
-                return new usonic.UltrasonicSensor(54, 0);
-            }, type, message);
+        assert.throwsError(function () {
+            return new UltrasonicSensor(54, 0);
+        }, type, message);
 
-            assert.throwsError(function () {
-                return new usonic.UltrasonicSensor(0.1, 0);
-            }, type, message);
+        assert.throwsError(function () {
+            return new UltrasonicSensor(0.1, 0);
+        }, type, message);
 
-            assert.throwsError(function () {
-                return new usonic.UltrasonicSensor(53.1, 0);
-            }, type, message);
-        });
+        assert.throwsError(function () {
+            return new UltrasonicSensor(53.1, 0);
+        }, type, message);
+    });
 
-        it('should not throw an invalid-trigger-pin error', function () {
-            var type = 'Error';
-            var message = 'Invalid trigger pin.';
+    it('should not throw an invalid-trigger-pin error', function () {
+        var type = 'Error';
+        var message = 'Invalid trigger pin.';
 
-            assert.doesNotThrowError(function () {
-                return new usonic.UltrasonicSensor(0, 0);
-            }, type, message);
+        assert.doesNotThrowError(function () {
+            return new UltrasonicSensor(0, 0);
+        }, type, message);
 
-            assert.doesNotThrowError(function () {
-                return new usonic.UltrasonicSensor(0, 53);
-            }, type, message);
-        });
+        assert.doesNotThrowError(function () {
+            return new UltrasonicSensor(0, 53);
+        }, type, message);
+    });
 
-        it('should throw an invalid-trigger-pin error', function () {
-            var type = 'Error';
-            var message = 'Invalid trigger pin.';
+    it('should throw an invalid-trigger-pin error', function () {
+        var type = 'Error';
+        var message = 'Invalid trigger pin.';
 
-            assert.throwsError(function () {
-                return new usonic.UltrasonicSensor(0, -1);
-            }, type, message);
+        assert.throwsError(function () {
+            return new UltrasonicSensor(0, -1);
+        }, type, message);
 
-            assert.throwsError(function () {
-                return new usonic.UltrasonicSensor(0, 54);
-            }, type, message);
+        assert.throwsError(function () {
+            return new UltrasonicSensor(0, 54);
+        }, type, message);
 
-            assert.throwsError(function () {
-                return new usonic.UltrasonicSensor(0, 0.1);
-            }, type, message);
+        assert.throwsError(function () {
+            return new UltrasonicSensor(0, 0.1);
+        }, type, message);
 
-            assert.throwsError(function () {
-                return new usonic.UltrasonicSensor(0, 53.1);
-            }, type, message);
-        });
+        assert.throwsError(function () {
+            return new UltrasonicSensor(0, 53.1);
+        }, type, message);
+    });
 
-        describe('.getDistanceCm()', function () {
+    describe('.getDistanceCm()', function () {
 
-            it('should throw an illegal-arguments error', function () {
-                assert.throwsError(function () {
-                    new usonic.UltrasonicSensor(0, 0).getDistanceCm(0);
-                }, 'TypeError', '(!>...) => number');
-            });
+        it('should call the corresponding mock funtion with pin-4 and pin-3 ' +
+            'as arguments and return its return value', function () {
+            var ultrasonicSensor = new UltrasonicSensor(4, 3);
+            var returnValue = {};
 
-            it('should return 1', function () {
-                var ultrasonicSensor = new usonic.UltrasonicSensor(4, 3);
+            mock.getDistanceCm = function (echoPin, triggerPin) {
+                assert.strictEqual(echoPin, 4);
+                assert.strictEqual(triggerPin, 3);
 
-                assert.strictEqual(ultrasonicSensor.getDistanceCm(), 1);
-            });
+                return returnValue;
+            };
 
-            it('should return -1', function () {
-                var ultrasonicSensor = new usonic.UltrasonicSensor(3, 4);
-
-                assert.strictEqual(ultrasonicSensor.getDistanceCm(), -1);
-            });
+            assert.strictEqual(ultrasonicSensor.getDistanceCm(), returnValue);
         });
     });
 });
