9bdd9d0ebd5cefa6c237591dc1fa4f16bba589b6
Clemens Akens <clebert@me.com>
2014-08-03 21:27:28 +0200

    Improve example

--- example/surveyor.js
+++ example/surveyor.js
@@ -16,32 +16,34 @@ var getInteger = function (name, defaultValue, callback) {
     });
 };
 
-getInteger('echoPin', 24, function (echoPin) {
-    getInteger('triggerPin', 23, function (triggerPin) {
-        getInteger('timeout', 450, function (timeout) {
-            rl.close();
+getInteger('Echo pin', 24, function (echoPin) {
+    getInteger('Trigger pin', 23, function (triggerPin) {
+        getInteger('Measurement timeout in µs', 450, function (timeout) {
+            getInteger('Measurement delay in ms', 60, function (delay) {
+                rl.close();
 
-            var cycles = 0;
-            var sensor = usonic.sensor(echoPin, triggerPin, timeout);
+                var cycles = 0;
+                var sensor = usonic.sensor(echoPin, triggerPin, timeout);
 
-            (function measure() {
-                setTimeout(function () {
-                    cycles += 1;
+                (function measure() {
+                    setTimeout(function () {
+                        cycles += 1;
 
-                    var distance = sensor();
+                        var distance = sensor();
 
-                    process.stdout.clearLine();
-                    process.stdout.cursorTo(0);
+                        process.stdout.clearLine();
+                        process.stdout.cursorTo(0);
 
-                    if (distance < 0) {
-                        process.stdout.write('[' + cycles + '] Error: Measurement timeout.\n');
-                    } else {
-                        process.stdout.write('[' + cycles + '] Distance: ' + distance.toFixed(2) + ' cm');
-                    }
+                        if (distance < 0) {
+                            process.stdout.write('[' + cycles + '] Error: Measurement timeout.\n');
+                        } else {
+                            process.stdout.write('[' + cycles + '] Distance: ' + distance.toFixed(2) + ' cm');
+                        }
 
-                    measure();
-                }, 20);
-            }());
+                        measure();
+                    }, delay);
+                }());
+            });
         });
     });
 });
